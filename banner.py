92d2bd7d3f08b00dc597ed5e271d07fbaf0c7995
Diogo S. Lourenço <114771411+diogolourenco100@users.noreply.github.com>
2024-05-19 20:25:21 -0300

    Create banner.py

---
+++ banner.py
@@ -0,0 +1,14 @@
+# Other fonts: http://www.figlet.org/examples.html
+
+import pyfiglet
+
+banner = pyfiglet.figlet_format("Banner", font="big")
+print(banner)
+
+# output:
+# ____
+#|  _ \
+#| |_) | __ _ _ __  _ __   ___ _ __
+#|  _ < / _` | '_ \| '_ \ / _ \ '__|
+#| |_) | (_| | | | | | | |  __/ |
+#|____/ \__,_|_| |_|_| |_|\___|_|
